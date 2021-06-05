8e0d210b261b26a174575ef07b859eb774456a9f
vidhu krishna <85400826+VidhuKrishna@users.noreply.github.com>
2021-06-05 19:54:05 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and react.js</h1>
+      <p>
+        ShapeAI has given students a wonderful opportunity to learn the basics of Javascript and react.js .This was an amazing bootcamp taken by Shaurya Sinha. We coverd
+        everything from Scratch including Javascript, React.js, html
+      </p>
+    </div>
+  );
+}
+
+export default Note;
